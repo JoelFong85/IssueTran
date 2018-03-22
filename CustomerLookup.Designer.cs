9e1443b188ee741e09786c234b4a23171f257092
Yong'En, Joel Fong <e0282870@u.nus.edu>
2018-03-22 17:24:20 +0800

    Add project files.

---
+++ CustomerLookup.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace IssueTran
+{
+    partial class CustomerLookup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CustGridVie = new System.Windows.Forms.DataGridView();
+            this.OkButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.CustGridVie)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CustGridVie
+            // 
+            this.CustGridVie.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustGridVie.Location = new System.Drawing.Point(22, 56);
+            this.CustGridVie.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.CustGridVie.Name = "CustGridVie";
+            this.CustGridVie.Size = new System.Drawing.Size(605, 326);
+            this.CustGridVie.TabIndex = 0;
+            // 
+            // OkButton
+            // 
+            this.OkButton.Location = new System.Drawing.Point(180, 441);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.OkButton.Name = "OkButton";
+            this.OkButton.Size = new System.Drawing.Size(138, 55);
+            this.OkButton.TabIndex = 1;
+            this.OkButton.Text = "Ok";
+            this.OkButton.UseVisualStyleBackColor = true;
+            this.OkButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(389, 441);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(138, 55);
+            this.CancelButton.TabIndex = 2;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // CustomerLookup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 503);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.OkButton);
+            this.Controls.Add(this.CustGridVie);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "CustomerLookup";
+            this.Text = "CustomerLookup";
+            ((System.ComponentModel.ISupportInitialize)(this.CustGridVie)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView CustGridVie;
+        private System.Windows.Forms.Button OkButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace IssueTran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CIDTextBox = new System.Windows.Forms.TextBox();
+            this.VideoCodeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.IssueDateDTP = new System.Windows.Forms.DateTimePicker();
+            this.DueDateDTP = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RemarksTextBox = new System.Windows.Forms.TextBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.CustLookupButton = new System.Windows.Forms.Button();
+            this.VideoLookupButton = new System.Windows.Forms.Button();
+            this.CustNameTextBox = new System.Windows.Forms.TextBox();
+            this.MovieTitleTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(120, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer ID";
+            // 
+            // CIDTextBox
+            // 
+            this.CIDTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CIDTextBox.Location = new System.Drawing.Point(252, 46);
+            this.CIDTextBox.Name = "CIDTextBox";
+            this.CIDTextBox.Size = new System.Drawing.Size(100, 31);
+            this.CIDTextBox.TabIndex = 1;
+            // 
+            // VideoCodeTextBox
+            // 
+            this.VideoCodeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VideoCodeTextBox.Location = new System.Drawing.Point(252, 131);
+            this.VideoCodeTextBox.Name = "VideoCodeTextBox";
+            this.VideoCodeTextBox.Size = new System.Drawing.Size(100, 31);
+            this.VideoCodeTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(120, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Video Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(120, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Issue Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(120, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Due Date";
+            // 
+            // IssueDateDTP
+            // 
+            this.IssueDateDTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IssueDateDTP.Location = new System.Drawing.Point(252, 211);
+            this.IssueDateDTP.Name = "IssueDateDTP";
+            this.IssueDateDTP.Size = new System.Drawing.Size(200, 31);
+            this.IssueDateDTP.TabIndex = 8;
+            // 
+            // DueDateDTP
+            // 
+            this.DueDateDTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DueDateDTP.Location = new System.Drawing.Point(252, 261);
+            this.DueDateDTP.Name = "DueDateDTP";
+            this.DueDateDTP.Size = new System.Drawing.Size(200, 31);
+            this.DueDateDTP.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(129, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Remarks";
+            // 
+            // RemarksTextBox
+            // 
+            this.RemarksTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemarksTextBox.Location = new System.Drawing.Point(252, 326);
+            this.RemarksTextBox.Multiline = true;
+            this.RemarksTextBox.Name = "RemarksTextBox";
+            this.RemarksTextBox.Size = new System.Drawing.Size(236, 92);
+            this.RemarksTextBox.TabIndex = 11;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(239, 437);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(77, 33);
+            this.SubmitButton.TabIndex = 12;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            // 
+            // CustLookupButton
+            // 
+            this.CustLookupButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustLookupButton.Location = new System.Drawing.Point(375, 46);
+            this.CustLookupButton.Name = "CustLookupButton";
+            this.CustLookupButton.Size = new System.Drawing.Size(42, 33);
+            this.CustLookupButton.TabIndex = 13;
+            this.CustLookupButton.Text = "...";
+            this.CustLookupButton.UseVisualStyleBackColor = true;
+            // 
+            // VideoLookupButton
+            // 
+            this.VideoLookupButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VideoLookupButton.Location = new System.Drawing.Point(375, 131);
+            this.VideoLookupButton.Name = "VideoLookupButton";
+            this.VideoLookupButton.Size = new System.Drawing.Size(42, 33);
+            this.VideoLookupButton.TabIndex = 14;
+            this.VideoLookupButton.Text = "...";
+            this.VideoLookupButton.UseVisualStyleBackColor = true;
+            // 
+            // CustNameTextBox
+            // 
+            this.CustNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustNameTextBox.Location = new System.Drawing.Point(252, 85);
+            this.CustNameTextBox.Name = "CustNameTextBox";
+            this.CustNameTextBox.ReadOnly = true;
+            this.CustNameTextBox.Size = new System.Drawing.Size(246, 31);
+            this.CustNameTextBox.TabIndex = 15;
+            // 
+            // MovieTitleTextBox
+            // 
+            this.MovieTitleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieTitleTextBox.Location = new System.Drawing.Point(252, 170);
+            this.MovieTitleTextBox.Name = "MovieTitleTextBox";
+            this.MovieTitleTextBox.ReadOnly = true;
+            this.MovieTitleTextBox.Size = new System.Drawing.Size(246, 31);
+            this.MovieTitleTextBox.TabIndex = 16;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(26, 546);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(949, 313);
+            this.tabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(8, 58);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(842, 146);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(8, 58);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(933, 247);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(22F, 44F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 881);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.MovieTitleTextBox);
+            this.Controls.Add(this.CustNameTextBox);
+            this.Controls.Add(this.VideoLookupButton);
+            this.Controls.Add(this.CustLookupButton);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.RemarksTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DueDateDTP);
+            this.Controls.Add(this.IssueDateDTP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.VideoCodeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CIDTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CIDTextBox;
+        private System.Windows.Forms.TextBox VideoCodeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker IssueDateDTP;
+        private System.Windows.Forms.DateTimePicker DueDateDTP;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox RemarksTextBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Button CustLookupButton;
+        private System.Windows.Forms.Button VideoLookupButton;
+        private System.Windows.Forms.TextBox CustNameTextBox;
+        private System.Windows.Forms.TextBox MovieTitleTextBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
